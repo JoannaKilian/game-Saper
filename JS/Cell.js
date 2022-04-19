45e5b759a0605ad82a323700f0045b647cd113f4
Joanna Borkala-Kilian <joanna.kilian@foundbycode.dev>
2022-04-19 15:01:01 +0200

    back to beter understand

--- JS/Cell.js
+++ JS/Cell.js
@@ -1,20 +1,24 @@
-import { UI } from "./UI.js"
+import { UI } from "./UI.js";
 
 export class Cell extends UI {
-    constructor(x, y) {
-        super();
-        this.x = x;
-        this.y = y;
-        this.value = 0;
-        this.isMine = false;
-        this.isReveal = false;
-        this.isFlagged = false;
-        this.selector = `[data-x="${this.x}"][data-y="${this.y}"]`;
-        this.element = null;
-    }
-
-    createElement() {
-        const element = `<div class="cell border border--concave" data-cell data-x="${this.x}" data-y="${this.y}"></div>`;
-        return element;
-    }
+  constructor(x, y) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.value = 0;
+    this.isMine = false;
+    this.isReveal = false;
+    this.isFlagged = false;
+    this.selector = `[data-x="${this.x}"][data-y="${this.y}"]`;
+    this.element = null;
+  }
+  createElement() {
+    const element = `<div class="cell border border--concave" data-cell data-x="${this.x}" data-y="${this.y}"></div>`;
+    return element;
+  }
+  revealCell() {
+    this.isReveal = true;
+    console.log(this.isReveal);
+    this.element.classList.remove("border--concave");
+  }
 }
--- JS/game.js
+++ JS/game.js
@@ -1,109 +1,86 @@
-import { Cell } from './Cell.js';
-import { UI } from './UI.js';
+import { Cell } from "./Cell.js";
+import { UI } from "./UI.js";
 
 class Game extends UI {
-    #config = {
-        easy: {
-            rows: 8,
-            cols: 8,
-            mines: 10
-        },
-        medium: {
-            rows: 16,
-            cols: 16,
-            mines: 40
-        },
-        expert: {
-            rows: 16,
-            cols: 30,
-            mines: 99
-        }
+  #config = {
+    easy: { rows: 8, cols: 8, mines: 10 },
+    medium: { rows: 16, cols: 16, mines: 40 },
+    expert: { rows: 16, cols: 30, mines: 99 },
+  };
+
+  #numberOfRows = null;
+  #numberOfCols = null;
+  #numberOfMines = null;
+  #cells = [];
+  #cellsElements = null;
+  #board = null;
+
+  initializeGame() {
+    this.#handleElements();
+    this.#newGame();
+  }
+
+  #newGame(
+    rows = this.#config.easy.rows,
+    cols = this.#config.easy.cols,
+    mines = this.#config.easy.mines
+  ) {
+    this.#numberOfCols = cols;
+    this.#numberOfMines = mines;
+    this.#numberOfRows = rows;
+
+    this.#setStyles();
+
+    this.#generateCells();
+    this.#renderBoard();
+    this.#cellsElements = this.getElements(this.UiSelectors.cell);
+    this.#addCellsEventListeners();
+  }
+
+  #handleElements() {
+    this.#board = this.getElement(this.UiSelectors.board);
+  }
+
+  #addCellsEventListeners() {
+    this.#cellsElements.forEach((element) => {
+      console.log("dzoała handle click");
+      element.addEventListener("click", this.#handleCellClick);
+      element.addEventListener("contextmenu", this.#handleCellContextMenu);
+    });
+  }
+
+  #generateCells() {
+    for (let row = 0; row < this.#numberOfRows; row++) {
+      this.#cells[row] = [];
+      for (let col = 0; col < this.#numberOfCols; col++) {
+        this.#cells[row].push(new Cell(col, row));
+      }
     }
+  }
+  #renderBoard() {
+    this.#cells.flat().forEach((cell) => {
+      this.#board.insertAdjacentHTML("beforeend", cell.createElement());
+      cell.element = cell.getElement(cell.selector);
+    });
+  }
+  #handleCellClick = (e) => {
+    const target = e.target;
+    const rowIndex = parseInt(target.getAttribute("data-y"), 10);
+    const colIndex = parseInt(target.getAttribute("data-x"), 10);
 
-    #numberOfRows = null;
-    #numberOfCols = null;
-    #numberOfMines = null;
-
-    #cells = [];
-    #cellsElements = null;
-
-    #board = null;
-
-    initializeGame() {
-        this.#handleElements();
-        this.#newGame();
-    }
-
-    #newGame(
-        rows = this.#config.easy.rows,
-        cols = this.#config.easy.cols,
-        mines = this.#config.easy.mines
-        ) {
-        this.#numberOfRows = rows;
-        this.#numberOfCols = cols;
-        this.#numberOfMines = mines;
-
-        this.#setStyles();
-
-        this.#generateCells();
-        this.#renderBoard();
-
-        this.#cellsElements = this.getElements(this.UiSelectors.cell);
-
-        this.#addCellsEventListeners;
-    }
-  
-
-    #handleElements() {
-        this.#board = this.getElement(this.UiSelectors.board);
-    }
-
-    #addCellsEventListeners() {
-        this.#cellsElements.forEach((element) => {
-            element.addEventListener('click', this.#handleCellClick);
-            element.addEventListener('contextmenu', this.#handleCellContextMenu)
-        })
-    }
-
-    #generateCells() {
-        // this.#cells.length = 0;
-        for (let row = 0; row < this.#numberOfRows; row++) {
-            this.#cells[row] = [];
-            for (let col = 0; col < this.#numberOfCols; col++) {
-                this.#cells[row].push(new Cell(col, row));
-            }
-        }
-    }
-
-  
-   
-    #renderBoard() {
-        this.#cells.flat().forEach((cell) => {
-            this.#board.insertAdjacentHTML('beforeend', cell.createElement());
-            cell.element = cell.getElement(cell.selector);
-        });
-    }
-
-    #handleCellClick = (e) => {
-        const target = e.target;
-        const rowIndex = parseInt(target.getAttribute('data-y'), 10)
-        const colIndex = parseInt(target.getAttribute('data-x'), 10)
-        
-
-    }
-
-
-    #setStyles() {
-        document.documentElement.style.setProperty(
-            '--cells-in-row',
-            this.#numberOfCols,
-            )
-    }
-
+    this.#cells[rowIndex][colIndex].revealCell();
+  };
+  #handleCellContextMenu = () => {};
+  #setStyles() {
+    document.documentElement.style.setProperty(
+      "--cells-in-row",
+      this.#numberOfCols
+    );
+  }
 }
 
 window.onload = function () {
-    const game = new Game();
+  const game = new Game();
 
-    game.initializeGame();
-}
+  game.initializeGame();
+};
